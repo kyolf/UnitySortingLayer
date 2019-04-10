845bfb7f4cb093b97fe131d4360c2e10a14d9650
Kyle Szeto <kyl3sz3t0@gmail.com>
2019-04-09 22:51:58 -0700

    got initial grid and blocks working

---
+++ SortingLayer/Assets/Scripts/BlockPiece.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPiece : MonoBehaviour
+{
+    private Vector2 offset;
+
+    //When Mouse is clicked
+    void OnMouseDown()
+    {
+        // Calculate offset of the mouse position and starting position of parent when drag begins
+        offset = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.parent.position;
+    }
+
+    //Dragging the block pieces
+    void OnMouseDrag()
+    {
+        Vector2 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.parent.position = newPosition - offset;
+    }
+}
